40910e7849972b6c2e106bb884b7652ec65b5ade
UmnaArshad <88517122+UmnaArshad@users.noreply.github.com>
2021-08-10 15:28:31 +0500

    Add files via upload

---
+++ meal.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<title>HealthCheck Meal Plans</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="page1.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<style>
+    .h1{
+        font-size: 24px;
+        font-style:italic;
+        color:black;
+    }
+    .h2{
+        text-align: center;
+        color: slategray;
+        font-size: 32px;
+    }
+</style>
+<body id="top">
+    <!-- Top Background Image Wrapper -->
+    <div class="bgded overlay" style="background-image:url('try.jpg');"> 
+      <!-- ################################################################################################ -->
+      <div class="wrapper row1">
+        <header id="header" class="hoc clear"> 
+          <!-- ################################################################################################ -->
+          <div id="logo" class="fl_left">
+            <h1><a href="#">HealthCheck</a></h1>
+          </div>
+          <nav id="mainav" class="fl_right">
+            <ul class="clear">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a class="drop" href="#">Diet</a>
+                <ul>
+                  <li><a href="page1.php">Weight Loss</a></li>
+                  <li><a href="page2.php">Weight Gain</a></li> 
+                </ul>
+              </li>
+              <li><a href="bmi.php">BMI Calculator</a></li>        
+              <li><a href="meal.php">Meal Plans</a></li>
+              <li><a href="signin.php">Sign Up</a></li>
+            </ul>
+          </nav>
+          <!-- ################################################################################################ -->
+        </header>
+      </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <section id="breadcrumb" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <h6 class="heading">HealthCheck Meal Plans</h6>
+    
+    </section>
+</div>
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content"> 
+      <!-- ################################################################################################ -->
+      <h3 style="font-size: 64px;"><b>Healthy Meal Plans You Can Order.</b></h2><br>
+      <img class="imgr borderedbox inspace-5" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F08%2F26230715%2F3749874.jpg&w=200&c=sc&poi=face&q=85" alt="">
+      <h2 class="h2">Meat and Seafood Options. Non Vegan Meals.</h2>
+      <p>
+        <h1 class="h1"><b> Beef & Bean Sloppy Joes</b></h1> This healthy copycat recipe of the comfort food classic trades beans for some of the meat to bump up fiber by 7 grams. We also cut back on the sugar and ketchup in this Sloppy Joe recipe makeover to save you 12 grams of added sugar.<br>
+        
+        <h1 class="h1"><b>Classic Sesame Noodles with Chicken</b></h1> Classic sesame noodles become a healthy meal with lean chicken and tons of veggies in this quick recipe for Asian noodles. Be sure to rinse the spaghetti until it's cold, then give it a good shake in the colander until it's well drained. Are you a spiralizing pro? Swap 5 cups of raw zucchini, carrot or other veggie “noodles” for the cooked pasta.<br>
+        
+        <h1 class="h1"><b>Beef Pad Thai</b></h1> We've bulked up the serving size of beef pad thai by adding in lots of healthy veggies like matchstick carrots, snap peas and scallions. Look for whole-grain brown-rice pad thai noodles to add an additional 3 grams fiber to each serving.<br>
+        
+        <h1 class="h1"><b>Beet & Shrimp Winter Salad</b></h1> This healthy dinner salad recipe gets its staying power from protein-packed shrimp and fiber-rich barley. With a simple red-wine vinaigrette, this quick salad makes just one serving but is easy to double or triple.<br>
+        
+        These dishes will come pre prepped with all necessary ingredients at your door step!<br></p>
+      
+      <img class="imgl borderedbox inspace-5" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F08%2F26232300%2F5694953.jpg&w=200&c=sc&poi=face&q=85" alt="">
+      <h2 class="h2"><b>Vegan Meal Plans</b></h2>
+      <p>Plant-based diets may help you lose weight. Vegetarianism and veganism are the most popular versions, which restrict animal products for health, ethical, and environmental reasons.
+
+        <h1 class="h1"><b>Vegan Coconut Chickpea Curry </b></h1> <p>To make this 20-minute vegan curry even faster, buy precut veggies from the salad bar at the grocery store. To make it a full, satisfying dinner, serve over cooked brown rice. When shopping for simmer sauce, look for one with 400 mg of sodium or less and check the ingredient list for cream or fish sauce if you want to keep this vegan. If you like a spicy kick, add a few dashes of your favorite hot sauce at the end.</p><br>
+        <p><h1 class="h1"><b>Moo Shu Vegetables </b></h1><p>This vegetarian version of the classic Chinese stir-fry, Moo Shu, uses already-shredded vegetables to cut down on the prep time. Serve with warm whole-wheat tortillas, Asian hot sauce and extra hoisin if desired.</p><br>
+        <h1 class="h1"><b>Spanakopita Scrambled Egg Pitas </b></h1><p>This meatless dinner comes together in only 15 minutes, thanks to the convenience of frozen chopped spinach and crumbled feta. We've added a flavorful burst of sun-dried tomato tapenade to spread over the whole-wheat pita before nestling the scrambled eggs inside. </p><br>
+        <h1 class="h1"><b>Chickpea Curry </b></h1><p>Made with convenient canned beans, this quick and healthy Indian recipe is an authentic chickpea curry that you can make in minutes. If you want an additional vegetable, stir in some roasted cauliflower florets. Serve with brown basmati rice or warm naan.</p><br>
+
+        <img class="imgl borderedbox inspace-5" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F08%2F26232606%2F5691772.jpg&w=200&c=sc&poi=face&q=85" alt="">
+        <h2 class="h2"><b>Pescetarian Meal Plans</b></h2>
+        
+          <h1 class="h1"><b>Spanakopita Scrambled Egg Pitas </b></h1> 
+          <p>This meatless dinner comes together in only 15 minutes, thanks to the convenience of frozen chopped spinach and crumbled feta. We've added a flavorful burst of sun-dried tomato tapenade to spread over the whole-wheat pita before nestling the scrambled eggs inside. If you can't find it, basil pesto or sun-dried tomato pesto also work well.</p><br>
+          <p><h1 class="h1"><b>Walnut-Rosemary Crusted Salmon</b></h1><p>Salmon and walnuts are both great sources of omega-3 fatty acids. Pair this easy salmon recipe with a simple salad and a side of roasted potatoes or quinoa.</p><br>
+          <h1 class="h1"><b>Mozzarella, Basil & Zucchini Frittata </b></h1>
+          <p>This vegetable-studded frittata recipe is one of the quickest meals you can make. Make it for breakfast, or serve for lunch or dinner with a tossed salad and a slice of olive oil-drizzled crusty baguette.</p><br>
+          <h1 class="h1"><b>Salmon Tacos with Pineapple Salsa </b></h1>
+          <p>An easy-to-make slaw served on the side of these salmon tacos makes for a filling and fast weeknight dinner. If you prefer more heat in these fish tacos, simply add a pinch or two of chipotle chile powder or cayenne with the chili powder.</p><br>
+      
+    
+    </div>
+</div>
+
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<div class="wrapper row4 bgded overlay">
+    <footer id="footer" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="one_third first">
+        <h6 class="heading">HEALTHWISE</h6>
+        <p>Thank you for visiting HEALTHWISE</p>
+        <p class="btmspace-50">Thankyou for being a part of our journey.</p>
+      </div>
+      <div class="one_third">
+        <h6 class="heading">FIND US!</h6>
+        <ul class="nospace btmspace-30 linklist contact">
+          <li><i class="fa fa-map-marker"></i>
+            <address>
+            New Street &amp; 34, Karachi, 3577/90
+            </address>
+          </li>
+          <li><i class="fa fa-phone"></i> +021 312 34567</li>
+          <li><i class="fa fa-fax"></i> +021 312 34566</li>
+          
+        </ul>
+      </div>
+      <!-- ################################################################################################ -->
+    </footer>
+  </div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="../layout/scripts/jquery.min.js"></script>
+<script src="../layout/scripts/jquery.backtotop.js"></script>
+<script src="../layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
